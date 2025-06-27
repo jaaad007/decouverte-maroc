0a8c4be27ed51d21101ceaa997064b757d8dc8c1
jad rafalia <jadrafalia15@gmail.com>
2025-06-27 11:17:30 +0200

    Fix about.js and add style for deployment

--- pages/about.js
+++ pages/about.js
@@ -9,10 +9,10 @@ export default function About() {
         Bienvenue sur <strong>Découverte du Maroc</strong> ! Ce blog a été réalisé dans le cadre d’un projet étudiant pour explorer trois villes emblématiques du Maroc.
       </p>
       <p className={styles.text}>
-        Le but est de montrer comment utiliser <strong>Next.js</strong> avec du contenu dynamique, des liens internes, et un design moderne.
+        L’objectif est d’appliquer les notions de développement web moderne avec <strong>Next.js</strong> : pages dynamiques, données externes (JSON), structure modulaire, déploiement sur Vercel...
       </p>
       <p className={styles.text}>
-        Merci pour votre visite !
+        Il s’agit d’un projet simple, mais professionnel, respectant les bonnes pratiques front-end et la mise en page responsive.
       </p>
       <Link href="/" className={styles.link}>← Retour à l'accueil</Link>
     </div>
